2d3763f991e7318cf4c038b431dc88a39ff34126
Anderson <samadhihuang@gmail.com>
2022-01-03 15:01:00 +0800

    Initial Commit

---
+++ Sources/Knob/Knob.swift
@@ -0,0 +1,6 @@
+public struct Knob {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Tests/KnobTests/KnobTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import Knob
+
+final class KnobTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(Knob().text, "Hello, World!")
+    }
+}
